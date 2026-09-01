ec83fb387f4c36734cf2e53c2417851fccd1a5ea
bsmcmath <becksm2@gmail.com>
2026-09-01 16:13:00 -0400

    Track only MessagerClient.exe

--- Start Client.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Double-click to open the Messager desktop client.
-cd /d "%~dp0"
-if not exist MessagerClient.exe (
-  echo MessagerClient.exe not found. Run build.bat first.
-  pause
-  exit /b 1
-)
-start "" MessagerClient.exe
--- dumpdep.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat" >nul
-dumpbin /dependents MessagerClient.exe
